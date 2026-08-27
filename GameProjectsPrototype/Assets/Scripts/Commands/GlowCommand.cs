4726b15f07d6abf90f12f784472f5a8b05930212
Bordocklius <miguel.dhaenens@hotmail.com>
2026-08-27 15:39:31 +0200

    Added Glow functionality to glowable component

---
+++ GameProjectsPrototype/Assets/Scripts/Commands/GlowCommand.cs
@@ -0,0 +1,25 @@
+﻿using Assets.Scripts.Commandables;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.Commands
+{
+    [CreateAssetMenu(fileName = "GlowCommand", menuName = "Commands/Glow Command")]
+    public class GlowCommand : CommandBase
+    {
+        public override bool CanExecuteCommand(GameObject target)
+        {
+            return target.TryGetComponent<Glowable>(out _);
+        }
+
+        public override void ExecuteCommand(GameObject target)
+        {
+            if (target.TryGetComponent<Glowable>(out Glowable glowable))
+            {
+                glowable.EnableGlow();
+            }
+        }
+    }
+}
